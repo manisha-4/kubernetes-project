cf4e6d1fa60d47be9ee42a9aa4991855c7a98771
Manisha Sah <manisha@Manishas-MacBook-Air.local>
2026-02-14 14:44:06 +0530

    deployment

---
+++ terraform/role-policy.tf
@@ -0,0 +1,14 @@
+resource "aws_eks_access_entry" "github_deployer" {
+  cluster_name = "eks-test-cluster"
+  principal_arn = "arn:aws:iam::990751731740:role/eks-deploy-role"
+}
+
+resource "aws_eks_access_policy_association" "github_deployer_admin" {
+  cluster_name = data.aws_eks_cluster.eks_test.name
+  principal_arn = aws_eks_access_entry.github_deployer.principal_arn
+  policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
